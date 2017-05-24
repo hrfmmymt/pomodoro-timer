a1798529e455261a85176a929c7426251064b39f
hrfmmymt <hrfmmymt@gmail.com>
2017-05-24 10:26:56 +0900

    modified

--- app/core.js
+++ app/core.js
@@ -1,26 +1,38 @@
+/**
+state = {
+  length:             (25min / 60000) [time in miliseconds]
+  is_active:          false [bool]
+  activity_type:      'p'   [string] p/b
+  timer:              (25min / 60000) [time in miliseconds] || -1
+}
+ */
+
 import { createStore, combineReducers } from 'redux'
 import * as reducers from './reducers'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 import App from './components/App'
 
 const store = createStore(
   combineReducers({
+    length: reducers.length,
+    is_active: reducers.is_active,
+    activity_type: reducers.activity_type,
+    timer: reducers.timer
   })
-);
+)
 
 const run = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <App/>
     </Provider>
     , document.getElementById('app')
   )
 }
 
 run()
-store.subscribe(run);
+store.subscribe(run)
 
-// *** DEBUG *** //
 window.store = store
